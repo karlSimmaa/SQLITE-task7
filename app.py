dfce980447debc810908ab49078aa32d8826ce1e
karlSimmaa <24254@burnside.school.nz>
2026-05-03 16:39:35 +1200

    second changge

--- app.py
+++ app.py
@@ -6,21 +6,18 @@ import sqlite3
 #contants and variables
 DATABASE = "fighters.db"
 
-
 #functions
 def print_all_aircraft():
-    
     """print all the aircraft nicely"""
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
     sql = "SELECT * from fighter;"
     cursor.execute(sql)
     results = cursor.fetchall()
-
     #loop through all the results
+    print(f"name    speed   max_g climb range payload")
     for fighter in results:
-        print(f"{fighter[1]}{fighter[2]}{fighter[3]}{fighter[4]}{fighter[5]}")
-
+        print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
     #loop finished here
     db.close()
 
