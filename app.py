d0f39a1aedf3d92bd04d985dd97d1be0e7861b63
karlSimmaa <24254@burnside.school.nz>
2026-05-04 10:34:46 +1200

    tuff

--- app.py
+++ app.py
@@ -49,18 +49,67 @@ def print_all_aircraft_by_g():
     #loop finished here
     db.close()
 
+def print_all_aircraft_by_climb():
+    """print all the aircraft by climb rate"""
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * from fighter ORDER BY climbrate DESC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print(f"name    speed   max_g climb range payload")
+    for fighter in results:
+        print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
+    #loop finished here
+    db.close()
+
+def print_all_aircraft_by_range():
+    """print all the aircraft by range"""
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * from fighter ORDER BY range DESC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print(f"name    speed   max_g climb range payload")
+    for fighter in results:
+        print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
+    #loop finished here
+    db.close()
+
+def print_all_aircraft_by_payload():
+    """print all the aircraft by payload"""
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * from fighter ORDER BY payload DESC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print(f"name    speed   max_g climb range payload")
+    for fighter in results:
+        print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
+    #loop finished here
+    db.close()
+
+
+
 #main code
 while True:
-    #get the user's input
-    user_input = input("\nWhat would you like to do.\n1. Print all aircraft\n7. Exit\n")
+    user_input = input("1. Print all aircraft\n2. Print all aircraft by speed\n3. Print all aircraft by max g\n4. Print all aircraft by climb rate\n5. Print all aircraft by range\n6. Print all aircraft by payload\n7. Exit\n")
 
 #conditions
     if user_input == "1":
         print_all_aircraft()
     elif user_input == "2":
-        pass
+        print_all_aircraft_by_speed()
     elif user_input == "3":
-        pass
+        print_all_aircraft_by_g()
+    elif user_input == "4":
+        print_all_aircraft_by_climb()
+    elif user_input == "5":
+        print_all_aircraft_by_range()
+    elif user_input == "6":
+        print_all_aircraft_by_payload()
     elif user_input == "7":
         break
     else:
